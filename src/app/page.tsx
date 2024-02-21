ae57d698607a7055a0ae1c7d1ed059023a99ea77
jhyun <jkwhd12@gmail.com>
2024-02-21 16:02:36 +0900

    design : Main 페이지 CSS 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+// tailwind - > 호불호 너무 심하고, 가독성 x
+// Styled Component -> Server Component SSR
+// sass
+// css module -> 간단하게 가자
+// vanilla extract -> Windows와 문제
+import Image from "next/image";
 import Link from "next/link";
 import zLogo from "../../public/zlogo.png";
 export default function Home() {
