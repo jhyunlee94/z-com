71dae3e93a6e76a4a531a9944b3be4d34840bbfc
jhyun <jkwhd12@gmail.com>
2024-02-22 14:13:54 +0900

    Initial commit

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -5,31 +5,14 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import zLogo from "@/../public/zlogo.png";
 import style from "@/app/page.module.css";
+import Main from "../_component/Main";
 // import { redirect } from "next/navigation";
 
 export default function Login() {
   // redirect("/i/flow/login");
   const router = useRouter();
   router.replace("/i/flow/login");
-  return (
-    <>
-      <div className={style.left}>
-        <Image src={zLogo} alt="logo" />
-      </div>
-      <div className={style.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-        <Link href="/i/flow/signup" className={style.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={style.login}>
-          {/* <Link href="/i/flow/login" className={style.login}> */}
-          로그인
-        </Link>
-      </div>
-    </>
-  );
+  return <Main />;
 }
 
 // router.push
--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -7,24 +7,7 @@ import style from "@/app/page.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import zLogo from "@/../public/zlogo.png";
+import Main from "./_component/Main";
 export default function Home() {
-  return (
-    <>
-      <div className={style.left}>
-        <Image src={zLogo} alt="logo" />
-      </div>
-      <div className={style.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-        <Link href="/i/flow/signup" className={style.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={style.login}>
-          {/* <Link href="/i/flow/login" className={style.login}> */}
-          로그인
-        </Link>
-      </div>
-    </>
-  );
+  return <Main />;
 }
