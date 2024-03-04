ee595d5942c94d785a4460b6ac8f25dcf8a8fdcc
jhyun <jkwhd12@gmail.com>
2024-03-04 16:15:10 +0900

    PostForm 만들기(타이핑 외우기)

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import style from "./home.module.css";
 import Tap from "./_component/Tap";
 import TabProvider from "./_component/TabProvider";
+import PostForm from "./_component/PostForm";
 export default function Home() {
   return (
     <main className={style.main}>
       <TabProvider>
         <Tap />
+        {/* Form 은 대부분 client 라고 생각하면 됨 */}
+        <PostForm />
       </TabProvider>
     </main>
   );
