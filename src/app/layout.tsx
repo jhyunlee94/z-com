7ceaa46fa6fa4787d50396ab5fa1afa4671e028a
jhyun <jkwhd12@gmail.com>
2024-03-05 16:01:38 +0900

    next용 msw 컴포넌트와 .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { MSWComponent } from "./_component/MSWComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,10 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MSWComponent />
+        {children}
+      </body>
     </html>
   );
 }
